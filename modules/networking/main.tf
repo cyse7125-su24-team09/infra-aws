a215f6889b0f77ca51ffc113d7957eb3849810a1
nitesh <more.ni@northeastern.edu>
2024-06-17 15:00:42 -0400

    feat: add AWS NAT gateway and private route table

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -5,16 +5,25 @@ resource "aws_vpc" "infra_aws_vpc" {
         Name = "infra-aws-vpc"
     }
 }
+resource "aws_internet_gateway" "infra_aws_igw" {
+    vpc_id = aws_vpc.infra_aws_vpc.id
 
-data "aws_availability_zones" "available" {}
+    tags = {
+        Name = "infra-aws-igw"
+    }
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
 
 resource "aws_subnet" "public_subnet" {
     count             = length(var.public_subnet_cidrs)
     vpc_id            = aws_vpc.infra_aws_vpc.id
     cidr_block        = var.public_subnet_cidrs[count.index]
-    availability_zone = data.aws_availability_zones.available[count.index].name
+    availability_zone = data.aws_availability_zones.available.names[count.index]
     map_public_ip_on_launch = true
-
+    depends_on = [aws_internet_gateway.infra_aws_igw]
     tags = {
         Name = "infra-aws-public-subnet-${count.index + 1}"
          
@@ -26,21 +35,35 @@ resource "aws_subnet" "private_subnet" {
     count             = length(var.public_subnet_cidrs)
     vpc_id            = aws_vpc.infra_aws_vpc.id
     cidr_block        = var.private_subnet_cidrs[count.index]
-    availability_zone = data.aws_availability_zones.available[count.index].name
+    availability_zone = data.aws_availability_zones.available.names[count.index]
     tags = {
         Name = "infra-aws-private-subnet-${count.index + 1}"
          
     }
 }
 
-resource "aws_internet_gateway" "infra_aws_igw" {
-    vpc_id = aws_vpc.infra_aws_vpc.id
-
+resource "aws_eip" "nat_eip" {
+  count    = length(var.public_subnet_cidrs)
+  domain   = "vpc"
     tags = {
-        Name = "infra-aws-igw"
-    }
+    Name = "infra-aws-nat-eip-${count.index}"
+  }
+  depends_on = [aws_internet_gateway.infra_aws_igw]
 }
 
+resource "aws_nat_gateway" "infra_aws_nat" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+
+ tags = {
+    Name = "infra-aws-nat-gateway-${count.index}"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.infra_aws_igw]
+}
 resource "aws_route_table" "public_route_table" {
     vpc_id = aws_vpc.infra_aws_vpc.id
     route {
@@ -51,13 +74,29 @@ resource "aws_route_table" "public_route_table" {
         Name = "infra-aws-public-route-table"
     }
 }
-
-resource "aws_route_table_association" "public_rta" {
+resource "aws_route_table_association" "public_rta_assc" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.infra_aws_route_table.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
+
+resource "aws_route_table" "private_route_table" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.infra_aws_vpc.id
+    route {
+        cidr_block = var.route_cidr_range
+        nat_gateway_id = aws_nat_gateway.infra_aws_nat[count.index].id
+    }
+    tags = {
+        Name = "infra-aws-private-route-table"
+    }
+}
+resource "aws_route_table_association" "private_rta_assc" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
 
 resource "aws_security_group" "eks" {
   vpc_id = aws_vpc.infra_aws_vpc.id
