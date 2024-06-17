b9769ba5bf0339f025c9d28b7aa0bef2023efdf3
nitesh <more.ni@northeastern.edu>
2024-06-17 13:53:14 -0400

    feat: inital aws vpc setup

---
+++ modules/networking/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "route_cidr_range" {
+    type = string
+    description = "CIDR range for the route table"
+    default = "0.0.0.0/0"
+}
