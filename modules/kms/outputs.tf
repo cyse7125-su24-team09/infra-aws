9147242f7a79680f6c7f77343d24af402d846bd9
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-06-18 00:27:19 -0400

    feat: :sparkles: added iam resources for eks cluster and node group

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -1,9 +1,9 @@
-output "eks_secrets_kms_key_id" {
-  description = "ID of the KMS key for encrypting EKS secrets"
-  value       = aws_kms_key.eks_secrets_cmek.id
+output "eks_secrets_kms_key_arn" {
+  description = "ARN of the KMS key for encrypting EKS secrets"
+  value       = aws_kms_key.eks_secrets_cmek.arn
 }
 
-output "ebs_kms_key_id" {
-  description = "ID of the KMS key for encrypting EBS volumes"
-  value       = aws_kms_key.ebs_cmek.id
+output "ebs_kms_key_arn" {
+  description = "ARN of the KMS key for encrypting EBS volumes"
+  value       = aws_kms_key.ebs_cmek.arn
 }
