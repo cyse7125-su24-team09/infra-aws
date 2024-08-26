16c8361fad8bb490b290dca41252df042e5f7e75
nitesh <more.ni@northeastern.edu>
2024-08-25 20:09:49 -0400

    feat: enable distributed tracing

--- modules/k8s/namespace/main.tf
+++ modules/k8s/namespace/main.tf
@@ -84,3 +84,13 @@ resource "kubernetes_namespace" "eck_elasticsearch" {
   }
 
 }
+resource "kubernetes_namespace" "llm_app" {
+
+  metadata {
+    labels = {
+      istio-injection = "enabled"
+    }
+    name = "llm-app"
+  }
+
+}
